5fe3d46531a88069fbf5faee9894e34871ef8669
sneghalathaterraform <santhoshonetouch29@gmail.com>
2026-07-21 22:19:11 -0400

    Rename EC2 instance tag to 'dev-server-2710'

--- main.tf
+++ main.tf
@@ -7,6 +7,6 @@ resource "aws_instance" "one-new" {
     ami = "ami-01edba92f9036f76e"
     instance_type = "t3.micro"
     tags = {
-        Name = "dev-server"
+        Name = "dev-server-2710"
     }
 }
